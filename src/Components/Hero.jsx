17ff50028f8fbb8d8c2d2a97902dc1841aa3b184
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-05-15 10:54:51 -0400

    commiting small menu changes & updates from end of last week

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,19 +3,47 @@ import '../Styles/Hero.css';
 import mango from '../Assets/Rectangle.png'
 import yellow from '../Assets/yellow.png'
 import { gsap } from 'gsap';
-import { ScrollToPlugin } from 'gsap/all';
+import { ScrollTrigger } from 'gsap/all';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 
 
 function Hero() {
-    gsap.registerPlugin(ScrollToPlugin);
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
     function click() {
         gsap.to(window, {duration: 1.25, scrollTo: {y: ".panel1", offsetY: 100}});
     }
 
     React.useEffect(() => {
         gsap.fromTo('.hero-text', {opacity: 0, y: 20, ease: 'back.out(3)'}, {duration: 1, y: 0, opacity: 1, });
+    //     gsap.to('.small-hero-text', { scrollTrigger: { 
+    //         trigger: ".hero",
+    //         start: "-100% top",
+    //         end: "30% top",
+    //         scrub: 0.8,
+    //         // markers: {startColor: "green", endColor: "red", fontSize: "12px"}
+    //     }, 
+    //     duration: 1, width: '100%', y: '100%'
+    // }
+    // )
+    gsap.fromTo('.small-hero-text, .larger-hero-text', { scrollTrigger: { 
+        trigger: ".hero",
+        start: "-100% top",
+        end: "30% top",
+        scrub: 0.8,
+        // markers: {startColor: "green", endColor: "red", fontSize: "12px"}
+    }, 
+    opacity: 0, y: 0, ease: 'back.out(3)'
+}
+, { scrollTrigger: {
+    trigger: ".hero",
+    start: "-100% top",
+    end: "30% top",
+    scrub: 0.8,
+    // markers: {startColor: "green", endColor: "red", fontSize: "12px"}
+}, duration: 1, y: -50, opacity: 1}
+)
     }, []);
 
   return (
