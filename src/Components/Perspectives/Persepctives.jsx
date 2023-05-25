c844af33397988ebe1d6742bd53371328e95b3ea
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-05-25 17:38:29 -0400

    attempting to add vars for contentful to deployment

--- src/Components/Perspectives/Persepctives.jsx
+++ src/Components/Perspectives/Persepctives.jsx
@@ -27,8 +27,8 @@ function Persepctives() {
       onComplete: fadeIn});
 
       const client = createClient({
-        space: process.env.REACT_APP_SPACE_ID,
-        accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+        space: REACT_APP_SPACE_ID,
+        accessToken: REACT_APP_ACCESS_TOKEN,
       });
 
 
